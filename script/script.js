e1dd493d377a048d5ce99742d855e35a02195863
10994015 <a0938599191@gmail.com>
2021-10-29 16:05:45 +0800

    10/29

--- script/script.js
+++ script/script.js
@@ -7,9 +7,35 @@ window.addEventListener("scroll",()=>{
     }
 })
 
-var menu = document.getElementsByClassName('menu')[0];
-var navlist = document.getElementsByClassName('navlist')[0];
+const menu = document.getElementsByClassName('menu')[0];
+const navlist = document.getElementsByClassName('navlist')[0];
+const newItemTitle = document.getElementsByClassName('newItemTitle');
+const newItemContent = document.getElementsByClassName('newItemContent');
+const newToggle = document.querySelectorAll('.newItemTitle >.fas');
 
+
+for(var w=0;w<newItemTitle.length;w++){
+    newItemTitle[w].addEventListener("click",openNew);
+}
+function openNew(){
+    var t = Number(this.className.substr(25));
+
+    console.log(!newItemContent[t].classList.contains('active'));
+    if(!newItemContent[t].classList.contains('active')){
+        removeNew();
+        newItemContent[t].classList.add('active');
+        newToggle[t].classList.add('fa-window-minimize');
+    }else{
+        removeNew();
+    }
+}
+
+function removeNew(){
+    for(var r=0;r<newItemContent.length;r++){
+        newItemContent[r].classList.remove('active');
+        newToggle[r].classList.remove('fa-window-minimize');
+    }
+}
 menu.addEventListener("click",()=>{
     
         navlist.style.display = "flex";
